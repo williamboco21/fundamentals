8c5b7fe9e67fa27d984139a6f8be3d08d683eb77
williamboco21 <williammboco@gmail.com>
2020-10-22 17:09:21 +0800

    try except and letter grade (conditionals)

---
+++ exercise_3.2.py
@@ -0,0 +1,22 @@
+try:
+    hours = float(input("Enter number of hours: "))
+    rate = float(input("Enter rate: "))
+except:
+    print("Error, please enter a numeric input.")
+    quit()
+
+
+print(hours, rate)
+base_hour = 40
+base_pay = base_hour * rate
+
+
+if hours > 40:
+    overtime_hours = hours - base_hour
+    overtime_pay = (overtime_hours * rate) * 1.5
+    pay = base_pay + overtime_pay
+
+else:
+    pay = base_pay
+
+print(pay)
